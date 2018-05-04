1558f4228acf82d351695048147889e0b2d168f3
dlinz7 <linzdiegoa@gmail.com>
2018-05-04 10:27:17 -0500

    Search Bar displaying along with broken images

---
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+ const SearchBar = () =>{
+
+
+        return(<div>
+
+
+                    <img src="img/insta-logo.png" alt="instaLogo"/>
+                    <img src="img/instagram-header.png" alt="instaHeader"/>
+                    <input 
+                    className="searchBar"
+                    type='text'
+                    placeholder="search"
+                    />
+                    <img src="img/heart.png" alt="iconHeart"/>
+                    <img src="img/person.png" alt="iconPerson"/>
+
+               </div>
+        )
+    }
+
+
+export default SearchBar;
